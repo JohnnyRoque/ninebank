5bb5197cd88dd476d3ab12c9e84cdaf054aad3f0
JohnnyRoque <johnnyroque.dev@gmail.com>
2023-09-28 15:00:52 -0300

    Added the icons and texts for the HomeFragment buttons,added new strings.

--- app/src/main/java/com/example/newninebank/model/DataSet.kt
+++ app/src/main/java/com/example/newninebank/model/DataSet.kt
@@ -5,13 +5,14 @@ import com.example.newninebank.R
 class DataSet {
     fun loadButtons(): List<DataModel> {
         return listOf(
-            DataModel(R.string.app_name, R.drawable.baseline_arrow_forward_ios_24),
-            DataModel(R.string.app_name, R.drawable.baseline_arrow_forward_ios_24),
-            DataModel(R.string.app_name, R.drawable.baseline_arrow_forward_ios_24),
-            DataModel(R.string.app_name, R.drawable.baseline_arrow_forward_ios_24),
-            DataModel(R.string.app_name, R.drawable.baseline_arrow_forward_ios_24),
-            DataModel(R.string.app_name, R.drawable.baseline_arrow_forward_ios_24),
-            DataModel(R.string.app_name, R.drawable.baseline_arrow_forward_ios_24)
+            DataModel(R.string.recharge_phone_text, R.drawable.smartphone_24px),
+            DataModel(R.string.payments_text, R.drawable.barcode_24px),
+            DataModel(R.string.pix_text, R.drawable.qr_code_2_24px),
+            DataModel(R.string.bank_loan_text, R.drawable.paid_24px),
+            DataModel(R.string.savings_text, R.drawable.savings_24px),
+            DataModel(R.string.transfer_text, R.drawable.currency_exchange_24px),
+            DataModel(R.string.financial_statement, R.drawable.account_balance_24px),
+            DataModel(R.string.investments_text, R.drawable.monitoring_24px)
 
         )
     }
